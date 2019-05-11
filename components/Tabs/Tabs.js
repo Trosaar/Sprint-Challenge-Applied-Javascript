1e3960d708d1c341c67e50174fb36629dd694746
Aaron T <troskaaaron@gmail.com>
2019-05-11 12:50:57 -0500

    MVP met. able to sort cards

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,14 +9,13 @@ class TabLink {
     console.log(this.tabData);
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:
     // Check to see if this.tabData is equal to 'all'
-    if(this.tabData = 'all'){
+    if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = this.tabData[`data-tab='${this.tabElement.dataset.tab}'`];
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabElement.dataset.tab}']`);
-      console.log(this.cards);
     }
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class.
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
@@ -33,11 +32,9 @@ class TabLink {
 
     // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
-    console.log(cards);
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => card.style.dispay = 'none');
-
+    Array.from(cards).forEach(card => card.style.display = 'none');
     // Add a class of ".active-tab" to this.tabElement
     this.tabElement.classList.add('active-tab');
 
